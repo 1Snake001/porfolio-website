3cd4e91160ffc708c9358605285077dc23edff4e
SnakeMacbookPro <snakemacbookpro@SnakeMacbookPro-iMac-Pro.local>
2023-05-24 16:23:53 +0200

    function that creates a navbar shadow for the scroll event was implemented

--- app/app.js
+++ app/app.js
@@ -1,11 +1,23 @@
+"use strict";
+
 // DOM Elements
-const toggleButton = document.querySelector(".toggle-button");
+const header = document.querySelector("header");
 const nav = document.querySelector("nav");
+const toggleButton = document.querySelector(".toggle-button");
 
 // The navbar and the toggle button switch
-function activeClassListSwitch(){
-    toggleButton.classList.toggle("active");
-    nav.classList.toggle("active");
-  };
+function activeClassListSwitch() {
+  toggleButton.classList.toggle("active");
+  nav.classList.toggle("active");
+}
 
-toggleButton.addEventListener('click', activeClassListSwitch);
+toggleButton.addEventListener("click", activeClassListSwitch);
+
+window.addEventListener("scroll", function () {
+  let windowScrollY = window.scrollY;
+  if (windowScrollY > 20) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
